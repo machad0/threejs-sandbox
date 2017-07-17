9a78ed5f7a6e5cf3d7e36ecb80d3ac4a1e6714fd
Lucas Machado <lucas.figueira@gmail.com>
2017-07-16 23:51:02 -0300

    feat: starting refactor to es6

---
+++ MainRefactor.js
@@ -0,0 +1,12 @@
+class MainCtrl {
+  constructor (camera, controls, scene, renderer, height, width, rendererIframe, sphere) {
+    this.camera = camera
+    this.controls = controls
+    this.scene = scene
+    this.height = height
+    this.width = width
+  }
+  init () {
+  }
+}
+module.exports = MainCtrl
